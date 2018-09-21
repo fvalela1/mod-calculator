76e53f78f58dbeb0a1e080bd268b635706fee6b0
Francesco Valela <valela.francesco@gmail.com>
2018-09-21 10:42:39 -0400

    initial commit of logic class

--- modcalculator/Calculator.swift
+++ modcalculator/Calculator.swift
@@ -1,4 +1,6 @@
 //
+//  This class does all calculation logic for the mod-calculator
+//
 //  Calculator.swift
 //  modcalculator
 //
@@ -6,4 +8,23 @@
 //  Copyright © 2018 Francesco Valela. All rights reserved.
 //
 
+
+/*
+ - does mod on two numbers
+ - checks that if the user clicks an operand after writing two numbers,
+ it calculates the mod of the result of the prior numbers and the new number
+ - makes sure you don't have two operands after one another
+ 
+*/
+
 import Foundation
+
+class Calculator {
+    
+    let formula = Stack<ItemModel>()
+    
+    func pushOperand(operand: Int) {
+        
+    }
+
+}
